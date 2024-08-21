889ca18cd4ed8b3c272e20c5fbb97ba1406a6573
Patiph4t <134424515+mou2548@users.noreply.github.com>
2024-08-21 16:35:32 +0700

    Create 20_string_operators_2.py

---
+++ elab01/20_string_operators_2.py
@@ -0,0 +1,21 @@
+# 1. Indexing (Access Index of String)
+# Operator: []
+
+# print('Python'[0])
+# ผลลัพธ์ที่ได้คือ 
+"P"
+# 1
+
+# subject = 'Fundamental Programming Concept'
+# text = subject[27] + subject[14] + subject[5] + subject[29] + subject[1] + subject[30] + subject[28] + subject[16]
+# print(text)
+# ผลลัพธ์ที่ได้คือ 
+"computer"
+# 1
+
+# subject = 'Fundamental Programming Concept'
+# text = subject[-22] * 5 + subject[0] * 0
+# print(text)
+# ผลลัพธ์ที่ได้คือ 
+"aaaaa"
+# 1
