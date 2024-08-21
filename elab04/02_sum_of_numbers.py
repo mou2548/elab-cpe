4adaf7c3965f8d5312d544671195ee215de06d15
Patiph4t <134424515+mou2548@users.noreply.github.com>
2024-08-21 16:53:13 +0700

    Create 02_sum_of_numbers.py

---
+++ elab04/02_sum_of_numbers.py
@@ -0,0 +1,13 @@
+positive, negative = 0, 0
+while True:
+    num = float(input("Enter a number (to exit, just enter 0): "))
+    if num == 0:
+        break
+    if num > 0:
+        positive += num
+    else:
+        negative += num
+
+print(f"The sum of positive numbers is {positive:.2f}")
+print(f"The sum of negative numbers is {negative:.2f}")
+    
