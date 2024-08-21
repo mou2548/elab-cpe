1324235ec1d2bfe5eb7c6042f9d92100dd374615
Patiph4t <134424515+mou2548@users.noreply.github.com>
2024-08-21 16:25:28 +0700

    Create 04_formatted_output_4.py

---
+++ elab01/04_formatted_output_4.py
@@ -0,0 +1,27 @@
+# ใช้ f"" เพื่อจัดรูปแบบ String (เฉพาะ Python 3.6 ขึ้นไป)
+# ให้ทดสอบคำสั่งต่อไปนี้ด้วย script mode แล้วตอบว่าแต่ละชุดคำสั่งให้ผลลัพธ์อะไร
+
+# ชุดคำสั่ง 1
+# price = 12.55
+# output = f"Price in Euro: {price}"
+# print(output)
+# ผลลัพธ์ของชุดคำสั่ง 1 คือ 
+"Price in Euro: 12.55"
+# 1
+
+# ชุดคำสั่ง 2
+# price = 12.54
+# output = f"Price in Swiss Franks: {price * 1.086}"
+# print(output)
+# ผลลัพธ์ของชุดคำสั่ง 2 คือ 
+"Price in Swiss Franks: 13.61844"
+# 1
+
+# ชุดคำสั่ง 3
+# price = 12.54
+# rate = 1.086
+# output = f"Price in Swiss Franks: {price * rate:10.2f}"
+# print(output)
+# ผลลัพธ์ของชุดคำสั่ง 3 คือ 
+"Price in Swiss Franks:      13.62"
+# 1
