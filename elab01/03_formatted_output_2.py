16b0f14ef72254d159458fcfb0a83bb2dc8f575d
Patiph4t <134424515+mou2548@users.noreply.github.com>
2024-08-21 16:22:46 +0700

    Create 03_formatted_output_2.py

---
+++ elab01/03_formatted_output_2.py
@@ -0,0 +1,33 @@
+# Formatted Output 2: String Concatenate Operator
+# ใช้เครื่องหมาย + ในการเชื่อม String ก่อน print
+
+# ให้ทดสอบคำสั่งต่อไปนี้ด้วย script mode แล้วตอบว่าแต่ละชุดคำสั่งให้ผลลัพธ์อะไร
+
+# ชุดคำสั่ง 1 เชื่อม String
+# output = "Hello" + "Python" + "is easy."
+# print(output)
+# ผลลัพธ์ของชุดคำสั่ง 1 คือ 
+"HelloPythonis easy."
+# 1
+
+# ชุดคำสั่ง 2 เชื่อมจำนวน
+# output = 34 + 24 + 35 + 170.0
+# print(output)
+# ผลลัพธ์ของชุดคำสั่ง 2 คือ 
+"263.0"
+# 1
+
+# ชุดคำสั่ง 3 เชื่อมจำนวน
+# output = str(34) + str(24) + str(35) + str(170.0)
+# print(output)
+# ผลลัพธ์ของชุดคำสั่ง 3 คือ 
+"342435170.0"
+# 1
+
+# ชุดคำสั่ง 4 เชื่อมระหว่างข้อมูลต่างชนิดกัน
+# age = 19
+# output = "I'm " + str(age) + " yrs."
+# print(output)
+# ผลลัพธ์ของชุดคำสั่ง 4 คือ 
+"I'm 19 yrs."
+# 1
