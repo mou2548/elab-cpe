b37356b9d316e29f8e5872bcc9b389e4a7f6e310
Patiph4t <134424515+mou2548@users.noreply.github.com>
2024-08-21 16:32:31 +0700

    Create 17_interest.py

---
+++ elab01/17_interest.py
@@ -0,0 +1,6 @@
+p = float(input("Input principal amount (P): "))
+r = float(input("Input annual nominal interest rate (r): "))
+n = float(input("Input # of times the interest is compounded per year (n): "))
+t = float(input("Input # of years (t): "))
+final = p * ((1 + r/n) ** (n*t))
+print("Final amount:", format(final, '.2f'))
