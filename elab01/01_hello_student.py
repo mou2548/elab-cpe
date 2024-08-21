1694091ce46f7bbd01fe7f7d5d88ed01acc6a4e3
Patiph4t <134424515+mou2548@users.noreply.github.com>
2024-08-21 16:18:35 +0700

    Create 01_hello_student.py

---
+++ elab01/01_hello_student.py
@@ -0,0 +1,15 @@
+"""
+Please tell me your name : siao 
+Hello, teacher and lovely TA..
+My name is siao
+siao is CPE#37 student..
+siao's favorite is Python.
+THANKS
+"""
+
+name = input("Please tell me your name : ")
+print('Hello, teacher and lovely TA..')
+print('My name is',name)
+print(name+ ' is CPE#37 student..')
+print(f"{name}'s favorite is Python.")
+print("THANKS")
