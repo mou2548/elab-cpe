f2a56c0a51d439dbc5d6007baf2a710dd12f5b1a
Patiph4t <134424515+mou2548@users.noreply.github.com>
2024-08-21 16:26:39 +0700

    Create 05_greet.py

---
+++ elab01/05_greet.py
@@ -0,0 +1,11 @@
+# What is your name?Clara
+# Hello, Clara.
+# What is your birth year?1990
+# You will be 20 this year.
+
+name = input("What is your name?")
+print("Hello,", name + '.')
+
+year = int(input("What is your birth year?"))
+age = 2010 - year
+print("You will be", str(age), "this year.")
